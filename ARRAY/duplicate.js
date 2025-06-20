47ac1b82ee53a19f9182c74d83c08fb3bebe657b
Dilna TK <dilnagangadharan@gmail.com>
2025-06-20 11:24:17 +0530

    array questions

--- ARRAY/duplicate.js
+++ ARRAY/duplicate.js
@@ -13,3 +13,21 @@ function findDuplicates(arr) {
     return [...new Set(duplicates)];
 }
 console.log(findDuplicates([1, 3, 4, 2, 1, 3, 5])); // Output: [1, 3]
+
+function findDuplicateChars(str) {
+    let map = {};
+    let duplicates = [];
+
+    for (let char of str) {
+        if (map[char]) {
+            map[char]++;
+            if (map[char] === 2) {
+                duplicates.push(char);
+            }
+        } else {
+            map[char] = 1;
+        }
+    }
+
+    return duplicates;
+}
