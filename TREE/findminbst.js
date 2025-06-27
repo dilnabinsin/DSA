a4fdf1250907b143de7faf3d897a403a17c8407a
Dilna TK <dilnagangadharan@gmail.com>
2025-06-27 07:09:57 +0530

    tree problems

--- TREE/findminbst.js
+++ TREE/findminbst.js
@@ -17,9 +17,14 @@ function findMin(root) {
     if (!root.left) return root.value;
     return findMin(root.left);
 }
-
+function findMax(root) {
+    if (!root.right) return root.value;
+    return findMax(root.right);
+}
 // Create BST
 let root = null;
 [50, 30, 70, 20, 40, 60, 80].forEach(val => root = insert(root, val));
 
 console.log("Minimum value in BST:", findMin(root));
+console.log(".............")
+console.log("max value in BST:", findMax(root));
