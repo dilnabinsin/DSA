081a7e3379f8c27adf6dd1b010d71369af717ebd
Dilna TK <dilnagangadharan@gmail.com>
2025-07-15 08:14:34 +0530

    Your commit message

--- typescript/oops/interface.ts
+++ typescript/oops/interface.ts
@@ -12,3 +12,22 @@ interface Person {
 class Student implements Person {
   constructor(public name: string, public age: number) {}
 }
+
+//Function Accepting User-defined Type
+interface User {
+  name: string;
+  age: number;
+}
+
+function printUser(user: User): void {
+  console.log(user.name, user.age);
+}
+
+// ✅ Create a User object
+const u1: User = {
+  name: "Dilna",
+  age: 25
+};
+
+// ✅ Call the function
+printUser(u1);
